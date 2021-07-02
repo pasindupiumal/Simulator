d4e25faffdff2260f98362bde08ccff2a5caf01f
Senevirathne L.U.P <it17177238@my.sliit.lk>
2021-07-02 14:31:37 +0530

    Currency conversion implementation

---
+++ Simulator/Models/Currency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Models
+{
+    class Currency
+    {
+        private string currencyCode;
+        private string currencyDesc;
+        private int decimals;
+
+        public Currency(string currencyCode, string currencyDesc, int decimals)
+        {
+            this.currencyCode = currencyCode;
+            this.currencyDesc = currencyDesc;
+            this.decimals = decimals;
+        }
+
+        public string CurrencyCode { get; set; }
+        public string CurrencyDesc { get; set; }
+        public int Decimals { get; set; }
+    }
+}
---
+++ Simulator/Shared/Currencies.cs
@@ -0,0 +1,57 @@
+﻿using Simulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Shared
+{
+    /// <summary>
+    /// Class for maintaining details of currencies
+    /// </summary>
+    static class Currencies
+    {
+        private static Dictionary<string, Currency> currencyList = null;
+
+        static Currencies()
+        {
+            currencyList = new Dictionary<string, Currency>();
+            AddCurrencies();
+        }
+
+        public static void AddCurrencies()
+        {
+            Currency SEK = new Currency("752", "SEK", 2);
+            Currency LKR = new Currency("144", "LKR", 2);
+            Currency EUR = new Currency("978", "EUR", 2);
+            Currency JPY = new Currency("392", "JPY", 0);
+            Currency USD = new Currency("840", "USD", 2);
+            Currency GBP = new Currency("826", "GBP", 2);
+
+            currencyList.Add(SEK.CurrencyCode, SEK);
+            currencyList.Add(LKR.CurrencyCode, LKR);
+            currencyList.Add(EUR.CurrencyCode, EUR);
+            currencyList.Add(JPY.CurrencyCode, JPY);
+            currencyList.Add(USD.CurrencyCode, USD);
+            currencyList.Add(GBP.CurrencyCode, GBP);
+        }
+
+        /// <summary>
+        /// Method for obtaining the currency details for a given currency code.
+        /// </summary>
+        /// <param name="currencyCode"></param>
+        /// <returns></returns>
+        public static Currency GetCurrency(string currencyCode)
+        {
+            if (currencyList.ContainsKey(currencyCode))
+            {
+                return currencyList[currencyCode];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
